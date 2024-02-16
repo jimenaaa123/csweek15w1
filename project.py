112f22d9e79242c3d778e7ce8ddd372902af8c83
chind <u5558810@live.warwick.ac.uk>
2024-02-16 10:47:52 +0000

    changing add_member code

--- project.py
+++ project.py
@@ -3,15 +3,14 @@ class Graph:
     def __init__(self):
         self.members = {}  #store members and relationships
 
-    def add_member(self, name, age):
-        self.name = name
-        self.age = age
-        self.friends = []
+    def add_member(self, name, age, location):
+        self.members[name] = {'age':age, 'location':location, 'friends':[]}
 
     def add_relationship(self, member1, member2):
-        member1.friends.append[member2] #add member 2 to the friend list of member1
+        self.members[member1]['friends'].append(member2)
 
-
+    def find_friends(self, member1):
+        return member1.friends
 
 
 network = Graph()
