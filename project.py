47b609f9b7381ce7db361c54b9aefddd620e0b72
ghasouhaaa <u5584703@live.warwick.ac.uk>
2024-02-16 10:58:06 +0000

    I added the add

--- project.py
+++ project.py
@@ -1,11 +1,19 @@
 
 class Graph:
+    self.Graph
     def __init__(self):
         self.members = {}  #store members and relationships
 
     def add_mem(self, name, age)
         self.name = name
         self.age = age
+
+    def find_friends(self, member1):
+        if member1 in self.friends:
+            return list(self.friends[member1]['friends'])
+        else:
+            print("Member not found in the network.")
+        return []
 
     def add_rel(self, member1, member2):
 
